358de78def37b9a2dc15d86cf0e7917e73ddd9a1
David Ly <dly@nerdery.com>
2015-04-08 11:03:15 -0500

    Initial commit with various Nuget packages

---
+++ AngularRequireSignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularRequireSignalR.Startup))]
+namespace AngularRequireSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            app.MapSignalR();
+        }
+    }
+}
